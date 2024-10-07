7c7a1d946129e0b2ddb7def84b4ec1e76ca298ad
Onalenna1 <128901368+Onalenna1@users.noreply.github.com>
2024-10-07 20:49:34 +0200

    Update Transaction.cs

--- CommBank-Server/Models/Transaction.cs
+++ CommBank-Server/Models/Transaction.cs
@@ -1,31 +1,44 @@
-﻿using MongoDB.Bson;
-using System.Text.Json.Serialization;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
-namespace CommBank.Models;
-
-public class Transaction
+namespace CommBank.Models
 {
-    [BsonId]
-    [BsonRepresentation(BsonType.ObjectId)]
-    public string? Id { get; set; }
+    public class Transaction
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
 
-    [JsonConverter(typeof(JsonStringEnumConverter))]
-    [BsonRepresentation(BsonType.String)]
-    public TransactionType TransactionType { get; set; }
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        [BsonRepresentation(BsonType.String)]
+        [Required]
+        public TransactionType TransactionType { get; set; }
 
-    public double Amount { get; set; } = 0.00;
+        [Range(0.01, double.MaxValue, ErrorMessage = "Amount must be greater than zero.")]
+        public double Amount { get; set; } = 0.00;
 
-    public DateTime DateTime { get; set; } = DateTime.Now;
+        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+        public DateTime DateTime { get; set; } = DateTime.UtcNow;
 
-    [BsonRepresentation(BsonType.ObjectId)]
-    public string? GoalId { get; set; }
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? GoalId { get; set; }
 
-    [BsonRepresentation(BsonType.ObjectId)]
-    public string? UserId { get; set; }
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? UserId { get; set; }
 
-    [BsonRepresentation(BsonType.ObjectId)]
-    public string[]? TagIds { get; set; }
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string[]? TagIds { get; set; }
 
-    public string? Description { get; set; }
+        [MaxLength(500)]
+        public string? Description { get; set; }
+
+        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
 }
