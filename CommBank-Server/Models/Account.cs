dbebc0df373fcb746622197b253df7f3e3d40721
Onalenna1 <128901368+Onalenna1@users.noreply.github.com>
2024-10-08 03:57:29 +0200

    Update Account.cs

--- CommBank-Server/Models/Account.cs
+++ CommBank-Server/Models/Account.cs
@@ -1,25 +1,49 @@
 ﻿using MongoDB.Bson;
-using System.Text.Json.Serialization;
 using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
-namespace CommBank.Models;
-
-public class Account
+namespace CommBank.Models
 {
-    [BsonId]
-    [BsonRepresentation(BsonType.ObjectId)]
-    public string? Id { get; set; }
+    public class Account
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
 
-    public long? Number { get; set; }
+        [Required(ErrorMessage = "Account number is required.")]
+        public long? Number { get; set; }
 
-    public string? Name { get; set; }
+        [Required(ErrorMessage = "Account name is required.")]
+        [MaxLength(100)] // Maximum length set as an example
+        public string? Name { get; set; }
 
-    public double Balance { get; set; } = 0;
+        [Range(0, double.MaxValue, ErrorMessage = "Balance must be non-negative.")]
+        public double Balance { get; set; } = 0;
 
-    [JsonConverter(typeof(JsonStringEnumConverter))]
-    [BsonRepresentation(BsonType.String)]
-    public AccountType AccountType { get; set; }
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        [BsonRepresentation(BsonType.String)]
+        public AccountType AccountType { get; set; }
 
-    [BsonRepresentation(BsonType.ObjectId)]
-    public List<string>? TransactionIds { get; set; }
+        [BsonRepresentation(BsonType.ObjectId)]
+        public List<string>? TransactionIds { get; set; } = new List<string>(); // Initialize to avoid null reference
+
+        // Optional constructor for easier initialization
+        public Account(long? number, string name, AccountType accountType)
+        {
+            Number = number;
+            Name = name;
+            AccountType = accountType;
+            TransactionIds = new List<string>();
+        }
+    }
+
+    public enum AccountType
+    {
+        Savings,
+        Checking,
+        Business,
+        // Add other account types as needed
+    }
 }
